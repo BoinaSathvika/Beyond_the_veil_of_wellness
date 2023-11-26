6245958e03600fa1d54312f7925671f6f8d9c6e4
BoinaSathvika <150325350+BoinaSathvika@users.noreply.github.com>
2023-11-26 15:24:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pickle
+import pandas
+import os
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+model = pickle.load(open(r'rfc.pkl', 'rb'))
+#scale = pickle.load(open(r'scale1.pkl','rb'))
+
+@app.route('/') # rendering the html template
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=["POST","GET"]) # rendering the html template
+def predict() :
+    return render_template("inner-page.html")
+
+@app.route('/submit',methods=["POST","GET"])# route to show the predictions in a web UI
+def submit():
+    #  reading the inputs given by the user
+    input_feature=[int(x) for x in request.form.values() ]  
+    #input_feature = np.transpose(input_feature)
+    input_feature=[np.array(input_feature)]
+    print(input_feature)
+    names = ['AnimalName', 'BloodBrainDisease', 'AppearenceDisease',
+       'GeneralDisease', 'LungDisease', 'AbdominalDisease']
+    data = pandas.DataFrame(input_feature,columns=names)
+    print(data)
+    
+    #data_scaled = scale.fit_transform(data)
+    #data = pandas.DataFrame(,columns=names)
+
+    
+
+     # predictions using the loaded model file
+    prediction=model.predict(data)
+    print(prediction)
+    prediction = int(prediction)
+    print(type(prediction))
+   
+    if (prediction == 0):
+       return render_template("output.html",result ="According to our study we feel sad to inform you that the animal is weak it requires immediate doctor consultation")
+    else:
+       return render_template("output.html",result = "You are health is in normal condition and you are good to go")
+     # showing the prediction results in a UI
+if __name__=="__main__":
+    app.run(debug = True, port = 1111)
